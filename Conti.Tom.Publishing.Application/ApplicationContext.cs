3c35f48f1c36c69ffe70bfd0856bc1f81db50a8d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-24 15:42:23 +0200

    IISBNService updated

--- Conti.Tom.Publishing.Application/ApplicationContext.cs
+++ Conti.Tom.Publishing.Application/ApplicationContext.cs
@@ -7,29 +7,28 @@ using Conti.Tom.Publishing.Books.IsbnGenerator.Interfaces;
 using Conti.Tom.Publishing.Books.Warehouse.Interfaces;
 using Conti.Tom.Publishing.Books.Warehouse.Services;
 
-namespace Conti.Tom.Publishing.Application
+namespace Conti.Tom.Publishing.Application;
+
+public static class ApplicationContext
 {
-    public static class ApplicationContext
+    private static IISBNService _ISBNService;
+    private static IStoreService _StoreService;
+    private static IBooksService _BooksService;
+    private static IConfigurationService _ConfigurationService;
+
+    static ApplicationContext()
     {
-        private static RandomISBNService _ISBNService;
-        private static IStoreService _StoreService;
-        private static IBooksService _BooksService;
-        private static IConfigurationService _ConfigurationService;
-
-        static ApplicationContext()
-        {
-            _ConfigurationService = new ConfigurationService();
-            var _ISBNService = new RandomISBNService();
-            _ISBNService.Prefix = _ConfigurationService.GetConfiguration("isbn.prefix");
-            _ISBNService.CountryCode = _ConfigurationService.GetConfiguration("isbn.countryCode");
-            _StoreService = new StoreService();
-            _BooksService = new BooksService(_ISBNService, _StoreService);
-            _StoreService.SetStock("books", new ISBN(4, 5, 6, 7), 100);
-        }
-
-        public static IISBNService IISBNService() { return _ISBNService; }
-        public static IStoreService IStoreService() { return _StoreService; }
-        public static IBooksService IBooksService() { return _BooksService; }
-
+        _ConfigurationService = new ConfigurationService();
+        _ISBNService = new RandomISBNService();
+        _ISBNService.Prefix = _ConfigurationService.GetConfiguration("isbn.prefix");
+        _ISBNService.CountryCode = _ConfigurationService.GetConfiguration("isbn.countryCode");
+        _StoreService = new StoreService();
+        _BooksService = new BooksService(_ISBNService, _StoreService);
+        _StoreService.SetStock("books", new ISBN(4, 5, 6, 7), 100);
     }
+
+    public static IISBNService IISBNService() { return _ISBNService; }
+    public static IStoreService IStoreService() { return _StoreService; }
+    public static IBooksService IBooksService() { return _BooksService; }
+
 }
--- Conti.Tom.Publishing.Books.IsbnGenerator/Interfaces/IISBNService.cs
+++ Conti.Tom.Publishing.Books.IsbnGenerator/Interfaces/IISBNService.cs
@@ -5,4 +5,6 @@ namespace Conti.Tom.Publishing.Books.IsbnGenerator.Interfaces;
 public interface IISBNService
 {
     ISBN Next();
+    public string Prefix { get; set; }
+    public string CountryCode { get; set; }
 }
