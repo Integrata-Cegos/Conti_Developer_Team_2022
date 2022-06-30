016b9e4fcff35aa32fb0e50a084d23407ab1c875
Stephan <stephan.kettner@continental-corporation.com>
2022-06-30 11:38:52 +0200

    change controller

--- DemoWebService/Controllers/WeatherForecastController.cs
+++ DemoWebService/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DemoWebService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("Wetter")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
