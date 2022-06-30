061db4cfd5892c51c3035d72336535ea05242741
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-30 13:49:09 +0200

    play with parameters and atributes

--- Store/Javacream.Store.Rest/Controllers/StoreWebServiceController.cs
+++ Store/Javacream.Store.Rest/Controllers/StoreWebServiceController.cs
@@ -7,9 +7,30 @@ namespace Javacream.Store.Rest.Controllers;
 public class StoreWebServiceController : ControllerBase
 {
 
-    [HttpGet("{category}/{item}")]
-    public String GetStock(string category, string item)
+    [HttpGet("path/{ItemCategory}/{ItemId}")]
+    public String GetStock([FromRoute(Name="ItemCategory")] string category, [FromRoute(Name="ItemId")]string item)
     {
+        Console.WriteLine($"retrieving stock for category={category} and item={item}");
         return "Stock: 42";
     }
+
+    [HttpGet("header")]
+    public String GetStockUsingHeaders([FromHeader(Name="ItemCategory")] string category, [FromHeader(Name="ItemId")] string item)
+    {
+        Console.WriteLine($"retrieving stock for category={category} and item={item}");
+        return "Stock: 42";
+    }
+    [HttpGet("query")]
+    public String GetStockUsingQuery([FromQuery(Name="ItemCategory")] string category, [FromQuery(Name="ItemId")] string item)
+    {
+        Console.WriteLine($"retrieving stock for category={category} and item={item}");
+        return "Stock: 42";
+    }
+    [HttpGet("mixed/{ItemId}")]
+    public String GetStockMixed([FromHeader(Name="ItemCategory")] string category, [FromRoute(Name="ItemId")] string item)
+    {
+        Console.WriteLine($"retrieving stock for category={category} and item={item}");
+        return "Stock: 42";
+    }
+
 }
