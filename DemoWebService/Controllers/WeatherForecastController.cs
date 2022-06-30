a660029aae16c52ab0cea77d97701e42e2f62edc
Stephan <stephan.kettner@continental-corporation.com>
2022-06-30 11:41:12 +0200

    add controller change_v2

--- DemoWebService/Controllers/WeatherForecastController.cs
+++ DemoWebService/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "gefroren", "bewölkt", "heiter", "kalt", "mild", "warm", "schatig", "heiß", "regnerisch", "gewitter"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
