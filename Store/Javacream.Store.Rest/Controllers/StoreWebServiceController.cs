b1ab86a3faac6d810154895b434460f20426eae3
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-30 15:17:39 +0200

    add Store Model

--- Store/Javacream.Store.Rest/Controllers/StoreWebServiceController.cs
+++ Store/Javacream.Store.Rest/Controllers/StoreWebServiceController.cs
@@ -1,24 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Javacream.Store.API;
 namespace Javacream.Store.Rest.Controllers;
 
 [ApiController]
 [Route("api/store")]
 public class StoreWebServiceController : ControllerBase
 {
-
+    private readonly IStoreService _storeService; 
+    public StoreWebServiceController(IStoreService storeService)
+    {
+        _storeService = storeService;
+    }
     [HttpGet("path/{ItemCategory}/{ItemId}")]
+    [Produces("text/plain")]
     public String GetStock([FromRoute(Name="ItemCategory")] string category, [FromRoute(Name="ItemId")]string item)
     {
         Console.WriteLine($"retrieving stock for category={category} and item={item}");
-        return "Stock: 42";
+        return _storeService.GetStock(category, item).ToString();
     }
 
     [HttpGet("header")]
-    public String GetStockUsingHeaders([FromHeader(Name="ItemCategory")] string category, [FromHeader(Name="ItemId")] string item)
+    public String GetStockUsingHeaders([FromHeader(Name="ItemCategory")] string category, [FromHeader(Name="ItemId")] string item, [FromServices] IStoreService iss)
     {
         Console.WriteLine($"retrieving stock for category={category} and item={item}");
-        return "Stock: 42";
+        return iss.GetStock(category, item).ToString();
     }
     [HttpGet("query")]
     public String GetStockUsingQuery([FromQuery(Name="ItemCategory")] string category, [FromQuery(Name="ItemId")] string item)
@@ -31,6 +36,11 @@ public class StoreWebServiceController : ControllerBase
     {
         Console.WriteLine($"retrieving stock for category={category} and item={item}");
         return "Stock: 42";
+    }
+    [HttpPost]
+    public void SetStock([FromHeader(Name="ItemCategory")] string category, [FromHeader(Name="ItemId")] string item, [FromHeader(Name="stock")] int stock){
+        Console.WriteLine($"retrieving stock for category={category} and item={item} and stock={stock}");
+        _storeService.SetStock(category, item, stock);
     }
 
 }
--- Store/Javacream.Store.Rest/Program.cs
+++ Store/Javacream.Store.Rest/Program.cs
@@ -1,12 +1,15 @@
+using Javacream.Store.API;
+using Javacream.Store.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//In älteren Versionen: public void ConfigureServices(... services){services.AddControllers()}
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IStoreService>(new StoreService());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
