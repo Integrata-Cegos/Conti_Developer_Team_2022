aa0d55933b93b1993c88af38898ec6fdca80f6cd
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-01 14:58:21 +0200

    streaming

--- Publishing/Javacream.Publishing.Application/Program.cs
+++ Publishing/Javacream.Publishing.Application/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Headers;
-
+using System.Collections.Generic;
+using System.Text.Json;
 
 public class Application
 {
@@ -19,5 +20,15 @@ public class Application
         Task<String> resultPromise = client.GetStringAsync("http://h2908727.stratoserver.net:8080/api/books");
         Console.WriteLine(resultPromise.Result);
 
+        var booksPromise = client.GetStreamAsync("http://h2908727.stratoserver.net:8080/api/books");
+        var data = booksPromise.Result;
+        var booksListPromise = JsonSerializer.DeserializeAsync<List<Book>>(data);
+        booksListPromise.Result.ForEach(book => Console.WriteLine(book.isbn));
+
+    }
+
+    class Book{
+        public string isbn {get; set;}
+        public string title {get; set;}
     }
 }
