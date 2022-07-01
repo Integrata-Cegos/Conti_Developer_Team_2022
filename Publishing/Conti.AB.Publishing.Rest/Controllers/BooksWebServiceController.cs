bf802083730aa0a59d857c2c52ad7d5519d36659
Anna-Lena Bleicher <Anna-Lena.Bauer@continental-corporation.com>
2022-07-01 09:49:49 +0200

    add UpdateBook function

--- Publishing/Conti.AB.Publishing.Rest/Controllers/BooksWebServiceController.cs
+++ Publishing/Conti.AB.Publishing.Rest/Controllers/BooksWebServiceController.cs
@@ -17,11 +17,41 @@ public class BooksWebServiceController : ControllerBase
         _booksService = booksService;
     }
 
-    [HttpGet("{isbn}")]
+    [HttpGet("byIsbn")]
     [Produces(MediaTypeNames.Application.Json)]
     public Book FindBookByIsbn([FromRoute(Name = "isbn")] Isbn isbn)
     {
         return _booksService.FindBookByIsbn(isbn);
+    }
+
+    [HttpPost]
+    public Isbn CreateBook([FromHeader(Name="title")] string title, [FromHeader(Name="pages")] int pages, [FromHeader(Name="price")] double price, [FromHeader(Name="options")] Dictionary<string, Object> options)
+    {
+        return _booksService.CreateBook(title, pages, price, options);
+    }
+
+    [HttpDelete]
+    public void DeleteBookByIsbn ([FromHeader(Name="isbn")] Isbn isbn)
+    {
+        _booksService.DeleteBookByIsbn(isbn);
+    }
+
+    [HttpGet("byTitle")]
+    public List<Book> FindBooksByTitle ([FromHeader(Name="title")] string title)
+    {
+        return _booksService.FindBooksByTitle(title);
+    }
+
+    [HttpGet("byPriceRange")]
+    public List<Book> FindBooksByPriceRange ([FromHeader(Name="minPrice")] double minPrice, [FromHeader(Name="maxPrice")] double maxPrice)
+    {
+        return _booksService.FindBooksByPriceRange(minPrice, maxPrice);
+    }
+
+    [HttpPut]
+    public void UpdateBook([FromHeader(Name="book")] Book book)
+    {
+        _booksService.UpdateBook(book);
     }
 } 
 
