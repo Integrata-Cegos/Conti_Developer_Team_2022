40dd3a57187591fd4a3cdefde127e874e0467e74
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-07-01 15:16:17 +0200

    scratch call ws

--- Publishing/Javacream.Publishing.Rest/Program.cs
+++ Publishing/Javacream.Publishing.Rest/Program.cs
@@ -17,13 +17,9 @@ builder.Services.AddSwaggerGen();
 var isbnService = new RandomIsbnService();
 isbnService.Prefix = "REST";
 isbnService.CountryCode = "dk";
-var storeService = new StoreService();
-storeService.SetStock("books", new Isbn(4,5,6,7), 100);
-storeService.SetStock("books", new Isbn(4,5,6,8), 10);
-var booksService = new BooksService(isbnService, storeService);
+var booksService = new BooksService(isbnService);
 builder.Services.AddSingleton<IIsbnService>(isbnService); 
 builder.Services.AddSingleton<IBooksService>(booksService); 
-builder.Services.AddSingleton<IStoreService>(storeService); 
 
 var app = builder.Build();
 
