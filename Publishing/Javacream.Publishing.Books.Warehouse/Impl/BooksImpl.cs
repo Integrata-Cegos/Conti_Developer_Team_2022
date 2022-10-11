92184159cba7e1ef2728adc335b7135d006f00b4
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-11 16:04:44 +0200

    scratch BooksImpl

--- Publishing/Javacream.Publishing.Books.Warehouse/Impl/BooksImpl.cs
+++ Publishing/Javacream.Publishing.Books.Warehouse/Impl/BooksImpl.cs
@@ -7,6 +7,7 @@ namespace Javacream.Books.Impl
 
     public class BooksService : IBooksService
     {
+        private publishingContext context = new publishingContext();
         public BooksService(IIsbnService isbnService, IStoreService storeService){
             this._isbnService = isbnService;
             this._storeService = storeService; 
@@ -15,7 +16,6 @@ namespace Javacream.Books.Impl
         private IStoreService _storeService;
         public Isbn CreateBook(string title, int pages, double price, Dictionary<string, Object> options)
         {
-            var context = new publishingContext();
             if (title == null)
             {
                 throw new ArgumentException("null title");
@@ -77,30 +77,30 @@ namespace Javacream.Books.Impl
 
         public Book FindBookByIsbn(Isbn isbn)
         {
-            Book book = this._books[isbn];
+            string isbnAsString = isbn.ToString();
+            Book book = assemble(context.Books.Single(b => b.Isbn == isbnAsString));
             this.SetAvailability(book);
             return book;
         }
         public void DeleteBookByIsbn(Isbn isbn)
         {
-            this._books.Remove(isbn);
+            string isbnAsString = isbn.ToString();
+            context.Books.Remove(context.Books.Single(b => b.Isbn == isbnAsString));
         }
 
         public List<Book> FindBooksByTitle(string title)
         {
-            var bookList = this._books.Values.ToList();
-            return bookList.FindAll(book => book.Title.Equals(title)).ConvertAll(book => SetAvailability(book));
+            return context.Books.Where(book => book.Title = title).ToList();
         }
         public List<Book> FindBooksByPriceRange(double minPrice, double maxPrice)
         {
-            var bookList = this._books.Values.ToList();
-            return bookList.FindAll(book => book.Price > minPrice && book.Price < maxPrice).ConvertAll(this.SetAvailability);
+            return context.Books.Where(book => book.Price > minPrice && book.Price < maxPrice).ToList();
         }
 
         public void UpdateBook(Book book)
         {
-            _books.Remove(book.Isbn);
-            _books.Add(book.Isbn, book);
+            context.Books.Add(assemble(book));
+            context.SaveChanges();
         }
         private Book SetAvailability(Book book)
         {
@@ -115,6 +115,40 @@ namespace Javacream.Books.Impl
             }
             return book;
         }
+
+        private Javacream.Publishing.Books.Warehouse.Entities.Book assemble(Book b)
+        {
+            var bookEntity = new Javacream.Publishing.Books.Warehouse.Entities.Book();
+            bookEntity.Isbn = b.Isbn.ToString();
+            bookEntity.Title = b.Title;
+            bookEntity.Pages = b.Pages;
+            bookEntity.Price = b.Price;
+            return bookEntity;
+
+        }
+        private Book assemble(Javacream.Publishing.Books.Warehouse.Entities.Book b)
+        {
+            string isbnAsString = b.Isbn;
+            string[] splitted = isbnAsString.Split(":");
+            string prefix = splitted[0];
+            string[] partsAndCountryCode = splitted[1].Split("-");
+            int part1 = Int32.Parse(partsAndCountryCode[0]);
+            int part2 = Int32.Parse(partsAndCountryCode[1]);
+            int part3 = Int32.Parse(partsAndCountryCode[2]);
+            int part4 = Int32.Parse(partsAndCountryCode[3]);
+            string countryCode = partsAndCountryCode[4];
+            Isbn isbn = new Isbn(prefix, countryCode, part1, part2, part3, part4);
+            var book = new Book(isbn, b.Title, (int)b.Pages, (double)b.Price, false);
+            return book;
+
+        }
+ 
+        private List<Book> assemble(List<Javacream.Publishing.Books.Warehouse.Entities.Book> books)
+        {
+            return books.Select(b => assemble(b)).ToList();
+
+        }
+ 
     }
 
 }
