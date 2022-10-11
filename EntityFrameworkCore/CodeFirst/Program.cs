4014c6c82d7b6d125585473a4ed54c7c0aece032
Anna-Lena Bleicher <Anna-Lena.Bleicher@continental.com>
2022-10-11 13:48:39 +0200

    add CodeFirst

---
+++ EntityFrameworkCore/CodeFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using Conti.AB.Efc;
+
+Console.WriteLine("Hello EntityFramework");
+BleiCatContext context = new BleiCatContext();
+BleiCat schnurri = new BleiCat("Schnurri", 9.99, "schwarz-weiß");
+Console.WriteLine(schnurri);
+context.BleiCats.Add(schnurri);
+Console.WriteLine(schnurri);
+context.SaveChanges();
+Console.WriteLine(schnurri);
+schnurri.Weight = 5.0;
+Console.WriteLine(schnurri);
+context.SaveChanges();
+context.BleiCats.Remove(schnurri);
+context.SaveChanges();
+List<BleiCat> catList = context.BleiCats.ToList();
+catList.ForEach(cat => Console.WriteLine(cat));
+BleiCat result = context.BleiCats.Single(cat => cat.Id == 4);
+Console.WriteLine(result);
